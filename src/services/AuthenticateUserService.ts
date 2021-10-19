83c30d2cc128ccd8cf6da2c52a6e457e52b91924
Luan Moraes <luansilvamoraess@gmail.com>
2021-10-19 02:53:50 -0300

    first commit

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+/**
+ * Receber code(string)
+ * Recuperar o acess_token no github
+ * Verificar se o usuario existe no DB
+ * ----- SIM = gera um token
+ * ----- NAO = cria no DB, gera um token
+ * Retornar o token com as infos do user
+ */
+
+class AuthenticateUserService {
+    async execute(code:string) {
+        const url = "https://github.com/login/oauth/acess_token";
+
+        const response = await axios.post(url, null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code,
+            },
+            headers: {
+                "Accept": "application/json"
+            }
+        })
+        return response.data;
+    }
+}
+
+export { AuthenticateUserService }
