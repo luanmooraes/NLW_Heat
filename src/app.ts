ddf7f711a83cfb49c41510a252a15e1a0b7dd753
Luan Moraes <luansilvamoraess@gmail.com>
2021-10-20 01:53:44 -0300

    Stage 01

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
 import express from 'express';
 import "dotenv/config";
+import http from "http";
+import cors from "cors";
+
+import { Server } from "socket.io";
+
 import {router} from './routes';
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*",
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+})
 
 app.use(express.json());
 
@@ -18,4 +36,4 @@ app.get('/signin/callback', (request, response) => {
     return response.json(code);
 })
 
-app.listen(4000, () => console.log('Server is running on PORT 4000'));
+export {serverHttp, io }
